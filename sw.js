2974f283bb6641054d2c562652a558da1e18d560
hsxiaobai <hsxb2024@163.com>
2025-01-18 22:26:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener("fetch", (evt) => {
-  if (evt.request.method === "GET" && event.request.url.includes("/data") {
-    event.respondWith(
+  if (evt.request.method === "GET" && evt.request.url.includes("/data")) {
+    evt.respondWith(
       new Response("Hello!", {
         headers: { "Content-Type": "text/plain" },
         status: 200,
         statusText: "OK"
       })
-    )
+    );
   }
 });
